ce9a3b8f6cd6e0f8c178e243e640478989fc2279
Kiran Mandhare <kiranmandhare22@gmail.com>
2025-09-26 17:18:40 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForbuild
+{
+    internal class Program
+    {
+     
+        public void Add()
+        {
+            Console.WriteLine("Hi Hello");
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
